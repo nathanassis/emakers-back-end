62f86557f398688dab1116f97231fba99d942e34
nathanassis <nathanfapereira@gmail.com>
2024-06-07 16:01:22 -0300

    initial commit

---
+++ src/main/java/com/emakers/trainee_back_end/TraineeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.emakers.trainee_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeBackEndApplication.class, args);
+	}
+
+}
