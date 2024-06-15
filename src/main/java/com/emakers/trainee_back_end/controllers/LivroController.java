30c2bc661fc39fe89802be975437145e74648f04
nathanassis <nathanfapereira@gmail.com>
2024-06-15 13:42:25 -0300

    feat: add swagger documentation

--- src/main/java/com/emakers/trainee_back_end/controllers/LivroController.java
+++ src/main/java/com/emakers/trainee_back_end/controllers/LivroController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.emakers.trainee_back_end.dtos.LivroDto;
 import com.emakers.trainee_back_end.models.LivroModel;
 import com.emakers.trainee_back_end.services.LivroService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,11 @@ public class LivroController {
     @Autowired
     LivroService livroService;
 
+    @Operation(description = "Cria um novo livro e retorna seus dados")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna o livro criado"),
+        @ApiResponse(responseCode = "400", description = "Retorna um log do erro")
+})
     @PostMapping
     public ResponseEntity<LivroModel> createLivro(@RequestBody @Valid LivroDto livroRequest) {
         var livroModel = new LivroModel();
@@ -30,16 +37,30 @@ public class LivroController {
         return ResponseEntity.status(HttpStatus.CREATED).body(livroService.create(livroModel));
     }
 
+    @Operation(description = "Retorna todos os livros do sistema")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna um array com todos os livros")
+})
     @GetMapping
     public ResponseEntity<List<LivroModel>> readAllLivros() {
         return ResponseEntity.status(HttpStatus.OK).body(livroService.getAll());
     }
 
+    @Operation(description = "Retorna as informações de um único livro")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna o livro com id passado"),
+        @ApiResponse(responseCode = "500", description = "Livro com id {} não foi encontrado")
+})
     @GetMapping("/{id}")
     public ResponseEntity<Object> readLivro(@PathVariable(value = "id") UUID id) {
         return ResponseEntity.status(HttpStatus.OK).body(livroService.validateAndGet(id));
     }
 
+    @Operation(description = "Atualiza as informações de um livro")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna o livro com os dados atualizados"),
+        @ApiResponse(responseCode = "404", description = "Livro com id {} não foi encontrado")
+})
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateLivro(@PathVariable(value = "id") UUID id,
             @RequestBody @Valid LivroDto livroRequest) {
@@ -48,6 +69,11 @@ public class LivroController {
         return ResponseEntity.status(HttpStatus.OK).body(livroService.create(livro));
     }
 
+    @Operation(description = "Deleta um livro do sistema")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "{nome} deletado(a) com sucesso."),
+        @ApiResponse(responseCode = "404", description = "Livro com id {} não foi encontrado")
+})
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteLivro(@PathVariable(value = "id") UUID id) {
         LivroModel livro = livroService.validateAndGet(id);
--- src/main/java/com/emakers/trainee_back_end/controllers/PessoaController.java
+++ src/main/java/com/emakers/trainee_back_end/controllers/PessoaController.java
@@ -15,6 +15,9 @@ import com.emakers.trainee_back_end.models.PessoaModel;
 import com.emakers.trainee_back_end.services.LivroService;
 import com.emakers.trainee_back_end.services.PessoaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +36,11 @@ public class PessoaController {
     @Autowired
     LivroService livroService;
 
+    @Operation(description = "Cria uma nova pessoa e retorna seus dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a pessoa criada"),
+            @ApiResponse(responseCode = "400", description = "Retorna um log do erro")
+    })
     @PostMapping
     public ResponseEntity<PessoaModel> createPessoa(@RequestBody @Valid PessoaDto pessoaRequest) {
         var pessoa = new PessoaModel();
@@ -40,16 +48,30 @@ public class PessoaController {
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.create(pessoa));
     }
 
+    @Operation(description = "Retorna todas as pessoas do sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um array com todas as pessoas")
+    })
     @GetMapping
     public ResponseEntity<List<PessoaModel>> readAllPessoas() {
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.getAll());
     }
 
+    @Operation(description = "Retorna as informações de uma única pessoa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a pessoa com id passado"),
+            @ApiResponse(responseCode = "500", description = "Pessoa com id {} não foi encontrada")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Object> readPessoa(@PathVariable(value = "id") UUID id) {
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.validateAndGet(id));
     }
 
+    @Operation(description = "Atualiza as informações de uma pessoa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna a pessoa com os dados atualizados"),
+            @ApiResponse(responseCode = "500", description = "Pessoa com id {} não foi encontrada")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Object> updatePessoa(@PathVariable(value = "id") UUID id,
             @RequestBody @Valid PessoaDto pessoaRequest) {
@@ -58,6 +80,11 @@ public class PessoaController {
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.create(pessoa));
     }
 
+    @Operation(description = "Deleta uma pessoa do sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "{nome} deletado(a) com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Pessoa com id {} não foi encontrada")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deletePessoa(@PathVariable(value = "id") UUID id) {
         PessoaModel pessoa = pessoaService.validateAndGet(id);
@@ -69,6 +96,12 @@ public class PessoaController {
     // -----
     // Emprestimo
 
+    @Operation(description = "Cria um novo empréstimo entre a pessoa e o livro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O livro de id {} foi emprestado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Livro com id {} não foi encontrado"),
+            @ApiResponse(responseCode = "500", description = "Pessoa com id {} não foi encontrada")
+    })
     @PostMapping("/{idPessoa}/livro/{idLivro}")
     public ResponseEntity<Object> createEmprestimo(@PathVariable(value = "idPessoa") UUID idPessoa,
             @PathVariable(value = "idLivro") UUID idLivro) {
@@ -77,6 +110,12 @@ public class PessoaController {
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.addLivro(pessoa, livro));
     }
 
+    @Operation(description = "Deleta um empréstimo do sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "O livro de id {} foi devolvido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Livro com id {} não foi encontrado"),
+            @ApiResponse(responseCode = "500", description = "Pessoa com id {} não foi encontrada")
+    })
     @DeleteMapping("/{idPessoa}/livro/{idLivro}")
     public ResponseEntity<Object> deleteEmprestimo(@PathVariable(value = "idPessoa") UUID idPessoa,
             @PathVariable(value = "idLivro") UUID idLivro) {
